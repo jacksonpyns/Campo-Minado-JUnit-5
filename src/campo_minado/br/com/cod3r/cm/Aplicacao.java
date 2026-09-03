72cf3b020dd2e65112a2413cb634ad198f1895e5
Jackson <limajackson115@gmail.com>
2026-09-03 13:03:35 -0300

    Classe Tabuleiro Console

--- src/campo_minado/br/com/cod3r/cm/Aplicacao.java
+++ src/campo_minado/br/com/cod3r/cm/Aplicacao.java
@@ -1,17 +1,14 @@
 package campo_minado.br.com.cod3r.cm;
 
 import campo_minado.br.com.cod3r.cm.modelo.Tabuleiro;
+import campo_minado.br.com.cod3r.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
 		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
 	}
 }
---
+++ src/campo_minado/br/com/cod3r/cm/excecao/SairException.java
@@ -0,0 +1,8 @@
+package campo_minado.br.com.cod3r.cm.excecao;
+
+public class SairException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
